4cc0b9aa76cfc42108d6ec40238a49ad601344a3
Jakub Brandys <kuba.brandys99@gmail.com>
2025-03-22 20:44:40 +0100

    MG-02: create component for tiles

---
+++ src/data/tiles.ts
@@ -0,0 +1,10 @@
+export const allTiles = [
+    { id: 'cat', image: '/assets/tiles/cat.png' },
+    { id: 'dog', image: '/assets/tiles/dog.png' },
+    { id: 'horse', image: '/assets/tiles/horse.png' },
+    { id: 'lion', image: '/assets/tiles/lion.png' },
+    { id: 'mouse', image: '/assets/tiles/mouse.png' },
+    { id: 'panda', image: '/assets/tiles/panda.png' },
+    { id: 'parrot', image: '/assets/tiles/parrot.png' },
+    { id: 'wolf', image: '/assets/tiles/wolf.png' },
+];
